ca138b8eb712890c59b4c49483d483e56cabd30c
Dayyan Shaikh <dayyanshaikh16@gmail.com>
2024-01-08 00:52:37 +0530

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
+import logo from './images/logo1.jpg'; 
+
 
 const Navbar = () => {
   return (
     <div>
       <header className="header">
         <nav>
-          <div className='logo'>EyeWear</div>
+          <div className='logo'>
+            <img src={logo} alt="image1" />
+            </div>
           <div className='nav'>
            <a href="#">Home</a>
            <a href="#">About</a>
            <a href="#">Shop</a>
-           <a href="#">Service</a>
+           <a href="#">Offers</a>
            <a href="#">Contact</a>
           </div>
-          <div className='cart'>Cart</div>
+          <div className='cart'>
+            Cart<i class="fa-1x fa-solid fa-cart-shopping"></i>
+            </div>
         </nav>
       </header>
       
