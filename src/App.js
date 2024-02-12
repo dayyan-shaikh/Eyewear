83f70fed62122295b4774e56d3400b2b19e16baf
Dayyan Shaikh <dayyanshaikh16@gmail.com>
2024-02-12 16:39:03 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Collection from './components/Collection/Collection'
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <>
       <Navbar />
       <Collection />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import './Footer.css'
+import { Col, Container, Row } from "react-bootstrap";
+
+const Footer = () => {
+  return (
+        <footer>
+          <Container>
+            <Row className="footer-row">
+              <Col md={3} sm={5} className="box">
+                <div className="logo">
+                  <ion-icon name="bag"></ion-icon>
+                  <h1>Arch EyeWare</h1>
+                </div>
+                <p>Get your favourate eye at MultiMart at very hood price</p>
+              </Col>
+              <Col md={3} sm={5} className="box">
+                <h2>About Us</h2>
+                <ul>
+                  <li>Terms & Conditions</li>
+                  <li>Privacy Policy</li>
+                </ul>
+              </Col>
+              <Col md={3} sm={5} className="box">
+                <h2>Customer Care</h2>
+                <ul>
+                  <li>Help Center </li>
+                  <li>How to Buy </li>
+                  <li>Track Your Order </li>
+                  <li>Returns & Refunds </li>
+                </ul>
+              </Col>
+              <Col md={3} sm={5} className="box">
+                <h2>Contact Us</h2>
+                <ul>
+                  <li>999 Ambernath West, Mumbai, India 421501</li>
+                  <li>Email: chai_pi_lo_friends@gmail.com</li>
+                  <li>Phone: +91 8989898989</li>
+                </ul>
+              </Col>
+            </Row>
+          </Container>
+        </footer>
+      );
+    };
+
+export default Footer
