81a6be0230bf321e75b5ba8305cab07c335555dd
Dayyan Shaikh <dayyanshaikh16@gmail.com>
2024-02-25 17:36:03 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,17 +2,11 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Collection from './components/Collection/Collection'
 import Footer from './components/Footer/Footer';
-// import {BrowserRouter,Routes ,Route} from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Navbar />
-      {/* <BrowserRouter>
-      <Routes>
-        <Route exact  path="/" ></Route>
-      </Routes>
-      </BrowserRouter> */}
+      <Navbar/>
       <Collection />
       <Footer />
       
